7fbbbd3ea2aa8aa250132eab8ef5f79f212c2daf
Mason Kim <masonkimsj@gmail.com>
2025-02-15 14:25:40 -0500

    add Tailwind CSS integration, update styles in LandingPage, and remove unused index.css

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -3,7 +3,7 @@ import { Container } from '../../styles/global'
 const Landing = () => {
 	return (
 		<Container id='landingPage'>
-			<h1>Mason Kim</h1>
+			<h1 className='text-3xl font-bold underline'>Mason Kim</h1>
 		</Container>
 	)
 }
